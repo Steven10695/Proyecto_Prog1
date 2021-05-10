f62e9c6040d45fb0b880e9f4f9db5829a9e7550c
Diego Linares <diegoalonsoll@hotmail.com>
2021-05-10 17:43:34 -0500

    Implementacion de Clases

---
+++ Models/Administrador.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_Prog1.Models
+{
+    public class Administrador
+    {
+        public string Id_Admin{get;set;}
+        public string Nombres_Admin{get;set;}
+        public string Correo_Admin{get;set;}
+        public string Contraseña_Admin{get;set;}
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,11 @@
+namespace Proyecto_Prog1.Models
+{
+    public class Producto
+    {
+        public string Id_Prod{get;set;}
+        public string Nombre_Prod{get;set;}
+        public double Cant_Prod{get;set;}
+        public string Categoria_Prod{get;set;}
+        public double Precio{get;set;} 
+    }
+}
