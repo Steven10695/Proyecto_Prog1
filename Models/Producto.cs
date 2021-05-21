7103faeb79d112f32c3812c412a69c63f76e109a
Diego Linares <diegoalonsoll@hotmail.com>
2021-05-20 20:33:29 -0500

    Creación de  cla}se Producto

---
+++ Models/Producto.cs
@@ -0,0 +1,11 @@
+namespace Proyecto_Prog1.Models
+{
+    public class Producto
+    {
+        public string Id_Producto{get;set;}
+        public string Nombre_Producto{get;set;}
+        public string Cantidad_Producto{get;set;}
+        public string Categoria_Producto{get;set;}
+        public string Precio_Producto{get;set;}
+    }
+}
