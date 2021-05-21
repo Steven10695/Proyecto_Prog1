5ca0ac4553d01be5ef4b804e7f2b942ff23efcfa
Diego Linares <diegoalonsoll@hotmail.com>
2021-05-20 20:30:27 -0500

    Creación de clase Administrador

---
+++ Models/Administrador.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Prog1.Models
+{
+    public class Administrador
+    {
+        public string Id_Admin { get; set;}
+        public string Correo_Admin{get;set;}
+        public string Contraseña_Admin{get; set;}
+    }
+}
