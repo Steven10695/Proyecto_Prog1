7336628aa178f4e265b345c894c0e14035cf9fb8
Erik Espinoza <erik_espinoza_mendoza@hotmail.com>
2021-05-26 21:10:20 -0500

    Insertados los Pre Contexts

---
+++ Models/IniciarSesion.cs
@@ -0,0 +1,7 @@
+namespace Proyecto_Prog1.Models{
+    public class IniciarSesion {
+        public int Id { get; set;}
+        public string Correo { get; set;}
+        public string Contraseña { get; set;}
+    } 
+}
