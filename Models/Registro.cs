116e13dba5dcac1bca04a6580558bcae3e23c2b7
Erik Espinoza <erik_espinoza_mendoza@hotmail.com>
2021-05-26 20:56:06 -0500

    Packetes framework y MySQL instalados

---
+++ Models/Registro.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_Prog1.Models{
+    public class Registro {
+        public int Id { get; set;}
+        public string Nombre { get; set;}
+        public string Correo { get; set;}
+        public string Contraseña { get; set;}
+        public string Telefono { get; set;}
+        public string Direccion { get; set;}
+    }
+}
