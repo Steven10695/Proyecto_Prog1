38f82a0743ad689f82af0c11b27ea9b448cee231
Alonso Gutelius <alonso_gutelius@usmp.pe>
2021-05-29 00:28:09 -0500

    añadiendo BD al startup

--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Proyecto_Prog1.Controllers;
 
 namespace Huerto_Del_valle
 {
@@ -24,6 +26,7 @@ namespace Huerto_Del_valle
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(dco => dco.UseInMemoryDatabase("DBConnection")); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
