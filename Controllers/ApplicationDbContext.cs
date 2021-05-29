1c10ba506de9cec8b64fb10ad8a910c1b8201f20
Alonso Gutelius <alonso_gutelius@usmp.pe>
2021-05-29 00:27:56 -0500

    Creación del db context

---
+++ Controllers/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Prog1.Models;
+
+namespace Proyecto_Prog1.Controllers
+{
+    public class ApplicationDbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+        public ApplicationDbContext(DbContextOptions p) : base (p)
+        {
+            
+        }
+    }
+}
