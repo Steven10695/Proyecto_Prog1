df162ac68e434ef46d4e4954d0c9d062ecc9e5f4
Diego Linares <diegoalonsoll@hotmail.com>
2021-05-29 20:42:15 -0500

    mejorando la conexion a bd

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Huerto_Del_valle.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,7 @@ namespace Huerto_Del_valle.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Producto> Productos { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
