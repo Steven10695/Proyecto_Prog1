c2f224d0dfe0ab08b675fcc4101881c5e4bad958
Diego Linares <diegoalonsoll@hotmail.com>
2021-06-02 15:30:37 -0500

    actualizacion de modelos

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,14 +4,14 @@ using System.Text;
 using Huerto_Del_valle.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Proyecto_Prog1.Models;
+
 
 namespace Huerto_Del_valle.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
         public DbSet<Producto> Productos { get; set; }
-        public DbSet<Cliente> Clientes { get; set; }
+     
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
