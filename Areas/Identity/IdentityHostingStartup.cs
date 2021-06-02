70c7bde2599a7f97130da9534170400ee3db5f99
Diego Linares <diegoalonsoll@hotmail.com>
2021-06-02 13:09:48 -0500

    creacion modelo cliente

--- Areas/Identity/IdentityHostingStartup.cs
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -1,11 +1,12 @@
 using System;
+using Huerto_Del_valle.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using MonoGamingStore.Data;
+
 
 [assembly: HostingStartup(typeof(Huerto_Del_valle.Areas.Identity.IdentityHostingStartup))]
 namespace Huerto_Del_valle.Areas.Identity
---
+++ Models/Cliente.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proyecto_Prog1.Models
+{
+    [Table("t_clientes")]
+    public class Cliente
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Id_Cliente")]
+        public int Id_Cliente{get;set;}
+
+        [Required(ErrorMessage="¡Espacio en blanco, Ingrese un Nombre!")]
+        [Display(Name="Ingresel Nombres")]
+        public string Nombres{get;set;}
+
+        [Required(ErrorMessage="¡Espacio en blanco, Ingrese un Apellido!")]
+        [Display(Name="Ingrese Apellidos")]
+        public string Apellidos{get;set;}
+
+        [Required(ErrorMessage="¡Espacio en blanco, Ingrese un Correo!")]
+        [Display(Name="Ingrese un correo")]
+        public string Correo{get;set;}
+
+        [Required(ErrorMessage="¡Espacio en blanco, Ingrese un Contraseña!")]
+        [Display(Name="Ingrese una Contraseña")]
+        public string Contraseña{get;set;}
+
+        [Required(ErrorMessage="¡Espacio en blanco, Ingrese un Teléfono!")]
+        [Display(Name="Ingrese un Teléfono")]
+        public string Telefono{get;set;}
+
+         [Required(ErrorMessage="¡Espacio en blanco, Ingrese una Direccion!")]
+        [Display(Name="Ingrese una Direccion")]
+        public string Direccion{get;set;}
+    
+    }
+}
