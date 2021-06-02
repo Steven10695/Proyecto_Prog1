3f1775b6dc9981865fb32956c8dbc2f791d5103a
Alonso Gutelius <alonso_gutelius@usmp.pe>
2021-06-02 17:10:13 -0500

    agregando quienes somos al controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -39,6 +39,10 @@ namespace Huerto_Del_valle.Controllers
         {
             return View();
         }
+         public IActionResult QuienesSomos()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
