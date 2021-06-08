fa6e92ee5297116dfbbdd54728461c61e924754a
Alonso Gutelius <alonso_gutelius@usmp.pe>
2021-06-08 10:41:30 -0500

    Creando model proforma

---
+++ Models/Proforma.cs
@@ -0,0 +1,25 @@
+using System; 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Huerto_Del_valle.Models;
+
+namespace Huerto_Del_valle.Models
+{
+    [Table("t_proforma")]
+    public class Proforma
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("Id")]
+        [Key]
+        public int id { get; set; }
+        [Display(Name="Usuario_Id")]
+        public String UserId { get; set; }
+        [Display(Name="Producto_Id")]
+        public Producto ProductoId { get; set; }
+        [Display(Name="Cantidad")]
+        public int Cantidad { get; set; }
+        [Display(Name="Precio")]
+        public double Precio { get; set; }
+    
+    }
+}
